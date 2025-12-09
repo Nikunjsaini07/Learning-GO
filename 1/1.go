69f48c99e4f814268059c7d3bcdcf7b0f13d24e9
Nikunj Saini <nikunj902782@gmail.com>
2025-12-09 21:57:57 +0530

    feat: some learning in go

---
+++ 1/1.go
@@ -0,0 +1,23 @@
+package main 
+import (
+	"fmt"
+)
+
+func main() {
+
+	// i:=0
+//  so basically a keynote in go is that if we  decalared a var in go we have to use it or we can't compile 
+
+     var arr [5]int 
+	 arr[0] = 1
+	 fmt.Print(arr[0])
+	 b:=[]int{1 ,2 ,3 }
+	 fmt.Print(len(b))
+
+	//  []int we have to use it as prefix if we directly assign values to a array 
+
+	
+
+	 
+	 
+}
