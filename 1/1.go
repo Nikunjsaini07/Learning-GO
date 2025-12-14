be8f42b343d8688fa84de01bb222726b459f5378
Nikunj Saini <nikunj902782@gmail.com>
2025-12-14 21:00:14 +0530

    feat: learn more about go syntax

--- 1/1.go
+++ 1/1.go
@@ -8,16 +8,48 @@ func main() {
 	// i:=0
 //  so basically a keynote in go is that if we  decalared a var in go we have to use it or we can't compile 
 
-     var arr [5]int 
-	 arr[0] = 1
-	 fmt.Print(arr[0])
-	 b:=[]int{1 ,2 ,3 }
-	 fmt.Print(len(b))
+    //  var arr [5]int 
+	//  arr[0] = 1
+	//  fmt.Print(arr[0])
+	//  b:=[]int{1 ,2 ,3 }
+	//  fmt.Print(len(b))
 
 	//  []int we have to use it as prefix if we directly assign values to a array 
 
-	
+//    temp := make([]int , 3 )
+//    fmt.Print(temp)
 
+// maps 
+
+// m := make(map[string] string )
+// m["name"] = "Nikunj"
+// fmt.Print(m["name"])
+
+// //  if key value does not exits it return empty string
+
+// delete(m , "name")
+
+// for delete
+//   fmt.Print(split(10 ))
+// //   so basically we can create a func in this format too 
+// // the x ,y are initialised in the begining and this  func automatically detects the values needs to be return 
+// 	//  we dont use this much 
 	 
-	 
-}
+// }
+//  func split(sum int )(x , y int ){
+// 	x = sum * 2 
+// 	y = sum + 10
+// 	return 
+
+//    i := 0
+//    for i < 10 {
+// 	 fmt.Println(i)
+// 	 i++
+//    }
+// the while loop
+
+
+
+
+ }
+
