9a00759b52de2a6928f1439de4db8f3116e1f2ff
Nikunj Saini <nikunj902782@gmail.com>
2025-12-21 17:21:51 +0530

    feat:
    learning about refrences

--- 1/1.go
+++ 1/1.go
@@ -52,7 +52,37 @@ func main() {
 // defer 
 //  for example you open a file (function calls ) and now after  some times you forgot to close that functions so what you did is you use defer added the function call to defer after calling it , it has a stack memeory which takes account of everything
 
+// type name struct{
+// 	x int 
+// 	y int 
+// }
+// fmt.Print(name{1,2 })
+// v := name{1 ,3 }
+// fmt.Print(v.x)
+
+// so this is sturct in go
 
 
- }
+// arr := [5]int{10, 20, 30, 40, 50}
+// 	s1 := arr[1:4] // 20,30,40
+// 	s2 := arr[2:]  // 30,40,50
+
+// 	fmt.Println("s1:", s1 , cap(s1))
+// 	fmt.Println("s2:", s2 , cap(s2))
+//  the zero value  slice is nil 
+
+// v:= []int{10 , 20 ,30 }
+// for i , m   := range v {
+// 	fmt.Println(i , "  " , m )
+// }
+
+
+// maps
+// var m map[int]int 
+// m = make(map[int]int)
+// m[10] = 90 
+// fmt.Print(m[10])
+
+
+}
 
